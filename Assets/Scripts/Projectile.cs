8d274653ad2b693605fafb4a201d0c7726170615
Blue-Flow <77975268+Blue-Flow@users.noreply.github.com>
2021-07-11 17:59:59 +0200

    ProjectileImplementation
    -Added projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Get the direction in which to move, and set the move function
+}
