d60c3dd37b737298b80dde98a02c0a1347eb2fad
Blue-Flow <77975268+Blue-Flow@users.noreply.github.com>
2021-07-12 19:39:41 +0200

    DefenderSpawnStarted
    - On mouse down in the play space, spawns a defender

--- Assets/Scripts/GlobalScripts/DefenderPlacement.cs
+++ Assets/Scripts/GlobalScripts/DefenderPlacement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class DefenderPlacement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] List<Defender> defendersPrefabs;
+    private Vector2 tileClickedPosition;
+    private void OnMouseDown()
     {
-        
+        tileClickedPosition = GetSelectedTile();
+        if (tileClickedPosition != Vector2.zero)
+            SpawnDefender();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void SpawnDefender()
     {
-        
+        Instantiate(defendersPrefabs[0], tileClickedPosition, Quaternion.identity);
+    }
+    private Vector2 GetSelectedTile()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit.collider != null)
+        {
+            return hit.collider.transform.position;
+        }
+        else return Vector2.zero;
     }
 }
