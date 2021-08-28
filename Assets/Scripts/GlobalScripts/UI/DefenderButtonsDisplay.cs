39f9ce98865956181bebf06113318c4e4582c7e7
Blue-Flow <77975268+Blue-Flow@users.noreply.github.com>
2021-08-28 14:40:40 +0200

    FlexibleButtonDisplay
    Buttons to spawn defenders adapting to level data
    End of refactoring regarding the level manager SO !

--- Assets/Scripts/GlobalScripts/UI/DefenderButtonsDisplay.cs
+++ Assets/Scripts/GlobalScripts/UI/DefenderButtonsDisplay.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class DefenderButtonsDisplay : MonoBehaviour
 {
-    [SerializeField] List<GameObject> ButtonList;
+    [SerializeField] GameObject Button_Amusement;
+    [SerializeField] GameObject Button_Joie;
+    [SerializeField] GameObject Button_Résilience;
+    [SerializeField] GameObject Button_Sérénité;
     private List<Defender> activeDefenderList;
 
     public void SetDefendersButtons(List<Defender> thisLevelDefenderList)
     {
         activeDefenderList = thisLevelDefenderList;
 
-        /*foreach(Defender defender in activeDefenderList)
+        foreach(Defender defender in activeDefenderList)
         {
-            switch(defender)
-            {
-                case defender == Defenders.Joie :
-                        Debug.Log("Joie");
-                    break;
-            }
-        }*/
+            if (defender.name == "Defender_Joie")
+                Button_Joie.SetActive(true);
+            if (defender.name == "Defender_Amusement")
+                Button_Amusement.SetActive(true);
+            if (defender.name == "Defender_Sérénité")
+                Button_Sérénité.SetActive(true);
+            if (defender.name == "Defender_Résilience")
+                Button_Résilience.SetActive(true);
+        }
     }
 }
